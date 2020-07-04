50833e7b108ccfe6181292f30c84d4f98d652fb0
unknown <angga.q.suwito@gmail.com>
2020-07-04 22:00:35 +0800

    [MODIFY]test conflict12345

--- main/app.go
+++ main/app.go
@@ -5,10 +5,8 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
+
 	allNames := []string{"Angga", "Dinda", "Mahdddddarani"}
-=======
-	allNames := []string{"Angga", "Dinda", "Maharani", "011198"}
->>>>>>> 2bbb0f479fa46ee7154e9f75da87ca3a8bc42425
+
 	name.InitName(allNames)
 }
